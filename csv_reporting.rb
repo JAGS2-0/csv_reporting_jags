6c75ba85ae5edd7306c1dc3869aa07671768a92f
jags2-0 <jessica@boomtownroi.com>
2017-01-16 15:03:49 -0500

    getting started

---
+++ csv_reporting.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+
+#
+# How much money did we make this week?
+# How much of a bonus did each employee get? (bonuses are paid to employees who pilot the Planet Express)
+# How many trips did each employee pilot?
+# How much money did we make broken down by planet? ie.. how much did we make shipping to Earth? Mars? Saturn? etc.
+#     Different employees have favorite destinations they always pilot to
+#
+# Fry - pilots to Earth (because he isn't allowed into space)
+# Amy - pilots to Mars (so she can visit her family)
+# Bender - pilots to Uranus (teeheee...)
+# Leela - pilots everywhere else because she is the only responsible one
